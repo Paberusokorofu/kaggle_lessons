e97256d8bf1a35b22b655718f42d4b86c76c9584
Paberusokorofu <8273482@mail.ru>
2025-09-29 09:27:33 +0300

    Second lesson is done

--- Functions.py
+++ Functions.py
@@ -1,4 +1,25 @@
-# Define the function
-def add_three(input_var):
-    output_var = input_var + 3
-    return output_var
+def get_pay (num_hours):
+    pay_pretax = num_hours * 15
+    pay_aftertax = pay_pretax * (1 - .12)
+    return pay_aftertax
+
+pay_fulltime = get_pay(40)
+print(pay_fulltime)
+
+def get_pay_with_more_inputs(num_hours, hourly_wage, tax_bracket) :
+    pay_pretax = num_hours * hourly_wage
+    pay_aftertax = pay_pretax * (1 - tax_bracket)
+    return pay_aftertax
+
+higher_pay_aftertax = get_pay_with_more_inputs(40, 15, .12)
+print(higher_pay_aftertax)
+
+
+# Define the function with no arguments and with no return
+def print_hello():
+    print("Hello, you!")
+    print("Good morning!")
+
+
+# Call the function
+print_hello()
