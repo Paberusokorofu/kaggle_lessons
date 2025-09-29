bdff8598c5955a4b943fa576ac8a75c059a914b3
Paberusokorofu <8273482@mail.ru>
2025-09-29 08:57:06 +0300

    adding 1 commit

--- Arithmetic and Variables.py
+++ Arithmetic and Variables.py
@@ -1,1 +1,10 @@
-print(1+2)
+# Create variables
+num_years = 4
+days_per_year = 365.25
+hours_per_day = 24
+minuts_per_hour = 60
+secs_per_min = 60
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
---
+++ Functions.py
@@ -0,0 +1,4 @@
+# Define the function
+def add_three(input_var):
+    output_var = input_var + 3
+    return output_var
