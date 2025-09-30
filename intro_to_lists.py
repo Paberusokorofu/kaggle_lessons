392ebcabda775c50d0d81572b65d5f553c5de9c6
Paberusokorofu <8273482@mail.ru>
2025-09-30 17:25:52 +0300

    ended all start courses

---
+++ intro_to_lists.py
@@ -0,0 +1,77 @@
+# flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold",
+#                 "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle"]
+#
+# print(len(flowers_list))
+# print("First entry:", flowers_list[0])
+# print("Second entry:", flowers_list[1])
+# print("Last entry:", flowers_list[9])
+#
+# print("First three entries:", flowers_list[:3])
+# print("Final two entries:", flowers_list[-2:])
+#
+# flowers_list.remove("globe thistle")
+# print(flowers_list)
+#
+# flowers_list.append("snapdragon")
+# print(flowers_list)
+
+# hardcover_sales = [139, 128, 172, 139, 191, 168, 170]
+#
+# print("Length of the list:", len(hardcover_sales))
+# print("Entry at index 2:", hardcover_sales[2])
+#
+# print("Minimum:", min(hardcover_sales))
+# print("Maximum:", max(hardcover_sales))
+#
+# print("Average books sold in first five days:", sum(hardcover_sales[:5])/5)
+#
+# print(type(hardcover_sales))
+#
+# print("Total books sold in one week:", sum(hardcover_sales))
+#
+# print("_________________")
+#
+# menu = ['stewed meat with onions', 'bean soup', 'risotto with trout and shrimp',
+#        'fish soup with cream and onion', 'gyro']
+#
+# menu.remove("bean soup")
+# menu.append("roasted beet salad")
+#
+# print(menu)
+
+# num_customers = [137, 147, 135, 128, 170, 174, 165, 146, 126, 159,
+#                  141, 148, 132, 147, 168, 153, 170, 161, 148, 152,
+#                  141, 151, 131, 149, 164, 163, 143, 143, 166, 171]
+#
+# avg_first_seven = sum(num_customers[:7]) / 7
+# avg_last_seven = sum(num_customers[-7:]) / 7
+# max_month = max(num_customers)
+# min_month = min(num_customers)
+#
+# print(avg_first_seven, avg_last_seven, max_month, min_month)
+
+# flowers = ("pink primrose,hard-leaved pocket orchid,canterbury bells,sweet pea,english marigold,tiger lily,moon orchid,"
+#            "bird of paradise,monkshood,globe thistle")
+#
+# print(flowers.split(","))
+
+# alphabet = "A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V.W.X.Y.Z"
+# address = "Mr. H. Potter,The cupboard under the Stairs,4 Privet Drive,Little Whinging,Surrey"
+#
+# letters = alphabet.split(".")
+# formatted_address = address.split(",")
+
+# print(letters)
+# print(formatted_address)
+
+# test_ratings = [1, 2, 3, 4, 5]
+# test_liked = [i>=4 for i in test_ratings]
+#
+# def percentage_liked(ratings):
+#     test_liked = [i>=4 for i in test_ratings]
+#     percentage_liked = ____
+#     return percentage_liked
+# print(test_liked)
+
+num_users_test = [920344, 1043553, 1204334, 1458996, 1503323, 1593432, 1623463, 1843064, 1930992, 2001078]
+
