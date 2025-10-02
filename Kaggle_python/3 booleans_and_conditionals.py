a148088ca6495c6d29139f8814263b981e0b81c3
Paberusokorofu <8273482@mail.ru>
2025-10-02 09:04:02 +0300

    5 lesson will start soon

--- Kaggle_python/3 booleans_and_conditionals.py
+++ Kaggle_python/3 booleans_and_conditionals.py
@@ -1,131 +1,131 @@
-# https://www.kaggle.com/code/colinmorris/booleans-and-conditionals
+https://www.kaggle.com/code/colinmorris/booleans-and-conditionals
 
-# def can_run_for_president(age):
-#     """Can someone of the given age run for president in the US?"""
-#     # The US Constitution says you must be at least 35 years old
-#     return age >= 35
-#
-# print("Can a 19-year-old run for president?", can_run_for_president(19))
-# print("Can a 45-year-old run for president?", can_run_for_president(45))
-#
+def can_run_for_president(age):
+    """Can someone of the given age run for president in the US?"""
+    # The US Constitution says you must be at least 35 years old
+    return age >= 35
 
-# print("_________________")
-#
-# def is_odd(n):
-#     return (n % 2) == 1
-#
-# print("Is 100 odd?", is_odd(100))
-# print("Is -1 odd?", is_odd(-1))
+print("Can a 19-year-old run for president?", can_run_for_president(19))
+print("Can a 45-year-old run for president?", can_run_for_president(45))
 
 
-# def can_run_for_president(age, is_natural_born_citizen):
-#     """Can someone of the given age and citizenship status run for president in the US?"""
-#     # The US Constitution says you must be a natural born citizen *and* at least 35 years old
-#     return is_natural_born_citizen and (age >= 35)
-#
-# print(can_run_for_president(19, True))
-# print(can_run_for_president(55, False))
-# print(can_run_for_president(55, True))
+print("_________________")
 
-# def inspect(x):
-#     if x == 0:
-#         print(x, "is zero")
-#     elif x > 0:
-#         print(x, "is positive")
-#     elif x < 0:
-#         print(x, "is negative")
-#     else:
-#         print(x, "is unlike anything I've ever seen...")
-#
-# inspect(0)
-# inspect(-15)
+def is_odd(n):
+    return (n % 2) == 1
 
-# def f(x):
-#     if x > 0:
-#         print("Only printed when x is positive; x =", x)
-#         print("Also only printed when x is positive; x =", x)
-#     print("Always printed, regardless of x's value; x =", x)
-#
-# f(1)
-# f(0)
+print("Is 100 odd?", is_odd(100))
+print("Is -1 odd?", is_odd(-1))
 
 
-# print(bool(1)) # all numbers are treated as true, except 0
-# print(bool(0))
-# print(bool("asf")) # all strings are treated as true, except the empty string ""
-# print(bool(""))
-# # Generally empty sequences (strings, lists, and other types we've yet to see like lists and tuples)
-# # are "falsey" and the rest are "truthy"
+def can_run_for_president(age, is_natural_born_citizen):
+    """Can someone of the given age and citizenship status run for president in the US?"""
+    # The US Constitution says you must be a natural born citizen *and* at least 35 years old
+    return is_natural_born_citizen and (age >= 35)
+
+print(can_run_for_president(19, True))
+print(can_run_for_president(55, False))
+print(can_run_for_president(55, True))
+
+def inspect(x):
+    if x == 0:
+        print(x, "is zero")
+    elif x > 0:
+        print(x, "is positive")
+    elif x < 0:
+        print(x, "is negative")
+    else:
+        print(x, "is unlike anything I've ever seen...")
+
+inspect(0)
+inspect(-15)
+
+def f(x):
+    if x > 0:
+        print("Only printed when x is positive; x =", x)
+        print("Also only printed when x is positive; x =", x)
+    print("Always printed, regardless of x's value; x =", x)
+
+f(1)
+f(0)
 
 
-# if 0:
-#     print(0)
-# elif "spam":
-#     print("spam")
+print(bool(1)) # all numbers are treated as true, except 0
+print(bool(0))
+print(bool("asf")) # all strings are treated as true, except the empty string ""
+print(bool(""))
+# Generally empty sequences (strings, lists, and other types we've yet to see like lists and tuples)
+# are "falsey" and the rest are "truthy"
 
 
-# def sign (x):
-#     if x > 0:
-#         return 1
-#     elif x < 0:
-#         return -1
-#     else:
-#         return 0
+if 0:
+    print(0)
+elif "spam":
+    print("spam")
 
 
-# def to_smash(total_candies):
-#     """Return the number of leftover candies that must be smashed after distributing
-#     the given number of candies evenly between 3 friends.
-#
-#     >>> to_smash(91)
-#     1
-#     """
-#     if total_candies == 1:
-#         print("Splitting 1 candy")
-#     else:
-#         print("Splitting", total_candies, "candies")
-#
-#
-# to_smash(91)
-# to_smash(1)
-
-# def prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday):
-#     # Don't change this code. Our goal is just to find the bug, not fix it!
-#     return (have_umbrella
-#             and rain_level > 5
-#             or have_hood
-#             or not rain_level > 0
-#             and is_workday)
-#
-# # Change the values of these inputs so they represent a case where prepared_for_weather
-# # returns the wrong answer.
-# have_umbrella = False
-# rain_level = 0.0
-# have_hood = False
-# is_workday = False
-
-# # Check what the function returns given the current values of the variables above
-# actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
-# print(actual)
+def sign (x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
 
 
-# def is_negative(number):
-#     return True if number < 0 else False
-# print(is_negative(5))
-# print(is_negative(0))
+def to_smash(total_candies):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
 
-# def wants_all_toppings(ketchup, mustard, onion):
-#     """Return whether the customer wants "the works" (all 3 toppings)
-#     """
-#     return ketchup and mustard and onion
-#     pass
+    >>> to_smash(91)
+    1
+    """
+    if total_candies == 1:
+        print("Splitting 1 candy")
+    else:
+        print("Splitting", total_candies, "candies")
 
-# def exactly_one_sauce(ketchup, mustard, onion):
-#     """Return whether the customer wants either ketchup or mustard, but not both.
-#     (You may be familiar with this operation under the name "exclusive or")
-#     """
-#     return (ketchup and not mustard) or (mustard and not ketchup)
-#     pass
+
+to_smash(91)
+to_smash(1)
+
+def prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday):
+    # Don't change this code. Our goal is just to find the bug, not fix it!
+    return (have_umbrella
+            and rain_level > 5
+            or have_hood
+            or not rain_level > 0
+            and is_workday)
+
+# Change the values of these inputs so they represent a case where prepared_for_weather
+# returns the wrong answer.
+have_umbrella = False
+rain_level = 0.0
+have_hood = False
+is_workday = False
+
+# Check what the function returns given the current values of the variables above
+actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
+print(actual)
+
+
+def is_negative(number):
+    return True if number < 0 else False
+print(is_negative(5))
+print(is_negative(0))
+
+def wants_all_toppings(ketchup, mustard, onion):
+    """Return whether the customer wants "the works" (all 3 toppings)
+    """
+    return ketchup and mustard and onion
+    pass
+
+def exactly_one_sauce(ketchup, mustard, onion):
+    """Return whether the customer wants either ketchup or mustard, but not both.
+    (You may be familiar with this operation under the name "exclusive or")
+    """
+    return (ketchup and not mustard) or (mustard and not ketchup)
+    pass
 
 def exactly_one_topping(ketchup, mustard, onion):
     """Return whether the customer wants exactly one of the three available toppings
--- Kaggle_python/4 Lists.py
+++ Kaggle_python/4 Lists.py
@@ -1,1 +1,81 @@
 # Stop here https://www.kaggle.com/code/colinmorris/lists
+
+primes = [2, 3, 5, 7]
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+hands = [
+    ['J', 'Q', 'K'],
+    ['2', '2', '2'],
+    ['6', 'A', 'K'], # (Comma after the last element is optional)
+]
+
+print(primes[2])
+print(planets[0:6])
+print(planets[1:])
+print(planets[:3])
+print(planets[1:-1])
+
+planets[3] = 'Malacandra'
+print(planets[0:6])
+
+planets[:3] = ['Mur', 'Vee', 'Ur']
+print(planets)
+
+print(len(planets))
+
+print(sorted(planets))
+
+x = 12
+# x is a real number, so its imaginary part is 0.
+print(x.imag)
+# Here's how to make a complex number, in case you've ever been curious:
+c = 12 + 3j
+print(c.imag)
+
+print(x.bit_length())
+
+planets.append('Earth')
+planets.append('Pluto')
+print(planets)
+
+help(planets.append)
+# planets.pop()
+print(planets)
+planets.index('Earth')
+
+print("Earth" in planets)
+
+#!!! help(planets)
+
+#Tuples
+
+t = (1, 2, 3)
+print(t)
+#They cannot be modified (they are immutable).
+
+x = 0.125
+x.as_integer_ratio()
+print(x)
+
+numerator, denominator = x.as_integer_ratio()
+print(numerator / denominator)
+
+a = 1
+b = 0
+a, b = b, a
+print(a, b)
+select_second = [1,2,3,4,5]
+
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    return L[1] if len(L) > 1 else None
+    pass
+
+
+
+
+
+
