886e1667794ca661da2d428bd77af079a81a208d
Paberusokorofu <8273482@mail.ru>
2025-10-04 13:41:22 +0300

    Dictionaries - is hard theme)

--- Kaggle_python/6 Strings_and_Dictionaries.py
+++ Kaggle_python/6 Strings_and_Dictionaries.py
@@ -1,2 +1,20 @@
 # https://www.kaggle.com/code/colinmorris/strings-and-dictionaries
 
+# x = 'Pluto\'s a planet!'
+# print(x)
+# What you type...	What you get	example	print(example)
+# \'	'	'What\'s up?'	What's up?
+# \"	"	"That's \"cool\""	That's "cool"
+# \\	\	"Look, a mountain: /\\"	Look, a mountain: /\
+# \n
+# "1\n2 3"	1
+# 2 3
+
+numbers = {'one':1, 'two':2, 'three':3}
+print(numbers['three'])
+numbers['one'] = 'Pluto'
+print(numbers['one'])
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+planet_to_initial = {planet: planet[0] for planet in planets}
+print(planet_to_initial)
